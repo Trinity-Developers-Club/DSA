a15d42ccba57715bc417b7b6d5a7dee6934a4a6d
Urvashi Saratkar <urvashisaratkar2004@gmail.com>
2023-10-26 08:07:31 +0530

    Arithmetic_operator

---
+++ CPP/Urvashi/arithmetic_operator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+    
+    int sum = a + b;
+    cout << sum << endl;
+    
+    int sub = a - b;
+    cout << sub << endl;
+    
+    int mul = a * b;
+    cout << mul << endl;
+    
+    int divi = a / b;
+    cout << divi << endl;
+    
+    int mod = a % b;
+    cout << mod << endl;
+    
+    return 0;
+}
