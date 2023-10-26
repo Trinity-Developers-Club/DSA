007942c2d0b01ee074fd8742c056d3716601d031
Urvashi Saratkar <urvashisaratkar2004@gmail.com>
2023-10-26 08:06:14 +0530

    Incre_decre_operator

---
+++ CPP/Urvashi/incre_decre_operator.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+
+    cout << a++ <<endl;   //post increment
+    
+    cout << ++a <<endl;    //pre increment 
+    
+    cout << --a <<endl;    //post decrement
+    
+    cout << a-- <<endl;     //pre decrement
+    
+    cout << (--a)-- <<endl;
+    
+    cout << (++a)++ <<endl;
+    
+    return 0;
+}
