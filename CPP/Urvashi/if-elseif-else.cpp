36649b76771f85f8c4bc99028befab1815068c67
Urvashi Saratkar <urvashisaratkar2004@gmail.com>
2023-10-26 08:00:33 +0530

    if-elseif-else

---
+++ CPP/Urvashi/if-elseif-else.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    
+    if(a>0){
+        cout<<"a is positive"<<endl;
+    }
+    else if(a<0){
+        cout<<"a is negative"<<endl;
+    }
+    else{
+        cout<<"a is zero"<<endl;
+    }
+    
+    return 0;
+}
