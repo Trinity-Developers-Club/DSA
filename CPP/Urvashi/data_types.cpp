fe93b3a44211ed226abedc756978ed7a9bd54908
Urvashi Saratkar <urvashisaratkar2004@gmail.com>
2023-10-26 07:58:46 +0530

    datatypes

---
+++ CPP/Urvashi/data_types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    cout << a <<" "<< sizeof(a) << endl;
+    
+    char b = 'a';
+    cout << b <<" "<< sizeof(b) << endl;
+    
+    float c = 1.2;
+    cout << c <<" "<< sizeof(c) <<endl;
+    
+    double d = 1.2333;
+    cout << d <<" "<< sizeof(d) <<endl;
+    
+    bool b1 = true;
+    cout << b1 <<" "<< sizeof(b1) <<endl;
+    
+    return 0;
+}
