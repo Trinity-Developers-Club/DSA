37dc83ac17f942798f972d4f03614513f94f0f36
Urvashi Saratkar <urvashisaratkar2004@gmail.com>
2023-10-26 08:02:59 +0530

    ternary_operator

---
+++ CPP/Urvashi/ternary_operator.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+
+    int ans = (a>b) ? (a+b) : (b-a);
+    
+    cout << ans <<endl;
+    
+    return 0;
+}
