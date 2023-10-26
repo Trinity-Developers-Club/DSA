5aa3d93108bd62282c0344a2be4fc1a89a4b05b8
Urvashi Saratkar <urvashisaratkar2004@gmail.com>
2023-10-26 08:13:06 +0530

    Create scope

---
+++ CPP/Urvashi/scopr.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+   int a=3;
+   cout<<a<<endl;
+
+   if(true){
+       int b=5;
+    cout<<b<<endl;
+   }
+   int b=8;
+   cout<<b<<endl;
+   
+   for(int b=1;<3;i++){
+       cout<<"hi"<<endl;
+   }
+
+    return 0;
+}
+
+
+//in a single block multiple value are not exist
+
+//if we want to create same variable in a block then 
+//we have to create another block in the existing block
+
+
