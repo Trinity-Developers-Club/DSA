3c05da8f3531a63de1ad6b0a2b552cc955aebaea
Devanshe sharma <devanshesharma6@gmail.com>
2023-10-31 08:33:39 +0530

    clone_graph
    Signed-off-by: Devanshe sharma <devanshesharma6@gmail.com>

---
+++ Python/Devanshe_2/clone_graph.py
@@ -0,0 +1,28 @@
+# Given a reference of a node in a connected undirected graph.
+
+# Return a deep copy (clone) of the graph.
+
+# Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
+
+# class Node {
+#     public int val;
+#     public List<Node> neighbors;
+# }
+ 
+ 
+def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node: return node
+        
+        q, clones = deque([node]), {node.val: Node(node.val, [])}
+        while q:
+            cur = q.popleft() 
+            cur_clone = clones[cur.val]            
+
+            for ngbr in cur.neighbors:
+                if ngbr.val not in clones:
+                    clones[ngbr.val] = Node(ngbr.val, [])
+                    q.append(ngbr)
+                    
+                cur_clone.neighbors.append(clones[ngbr.val])
+                
+        return clones[node.val]
